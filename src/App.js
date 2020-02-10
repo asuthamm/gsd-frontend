b250a93f8ab18db18ee26e262d709c6bee3abff1
bigfishh <anniezheng.anniee@gmail.com>
2020-02-10 10:31:17 -0500

    react routing added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
+import {Route, Switch} from 'react-router';
+import {Link, NavLink} from 'react-router-dom'
+import RegisterLoginContainer from './components/RegisterLoginContainer'
+import Form from './components/Form';
+
+
 
 class App extends Component {
 
@@ -17,11 +23,23 @@ class App extends Component {
       }))
   }
 
+  renderForm = (routerProps) => {
+    if(routerProps.location.pathname === "/login"){
+      return <Form formName="Login Form" handleSubmit={this.handleLoginSubmit}/>
+    } else if (routerProps.location.pathname === "/register") {
+      return <Form formName="Register Form" handleSubmit={this.handleRegisterSubmit}/>
+    }
+  }
+
   render() {
     console.log(this.state)
     return (
       <div>
-        <Home />
+        <Switch>
+          <Route path="/home" render={this.renderHome}/>
+          <Route path="/login" render={this.renderForm}/>
+          <Route path="/register" render={this.renderForm}/>
+        </Switch>
       </div>
     );
   }
---
+++ src/components/RegisterLoginContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Form from './Form';
+
+class RegisterLoginContainer extends Component {
+    render() {
+        return (
+        <div>
+            <p>Login/Register Form</p>
+            <Form/>
+        </div>
+        );
+    }
+}
+
+export default RegisterLoginContainer;
