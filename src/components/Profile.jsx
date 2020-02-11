74e8126e5fcc0508ddf729b989b4ebaa510282af
asuthamm <asuthamm@gmail.com>
2020-02-11 15:09:26 -0500

    daily pushed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
+
   render() {
+    const { user } = this.props
+    const { user:innerUser, token } = user
+    const {username, name, bio, img_url} = innerUser
+    console.log(username, name, bio, img_url)
+    // const {}
     return (
       <div>
-        hi from Profile!!!
+        hi {this.props.user.user.username} from Profile!!!
       </div>
     );
   }
